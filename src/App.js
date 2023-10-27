8ad442ee1920ae6299cc2edb0e69811957cb6576
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-10-27 09:08:41 -0700

    will lift the state of search

--- src/App.js
+++ src/App.js
@@ -1,81 +1,72 @@
-import React from "react";
+import React from 'react';
 
-function List(props) {
-  return (
-    <div>
-      <h2>{props.title}</h2>
-      <ul>
-        {props.list.map(function (item) {
-          return <Item item={item} />;
-        })}
-      </ul>
-    </div>
-  );
-}
+function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const Item = (props) => {
-  return <li key={props.item.id}>{props.item.title}</li>;
-};
+  const [searchTerm, setSearchTerm] = React.useState('');
 
-function Search(props) {
-  const [searchTerm, setSearchTerm] = React.useState("");
-
-  const handleChange = (event) => {
+  const handleSearch = (event) => {
+    console.log(event.target.value);
     setSearchTerm(event.target.value);
-    props.onSearch(event);
   };
 
-  const handleMouseOver = (event) => {
-    console.log(event);
-  };
+  const searchStories = stories.filter((story) =>
+    story.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+  );
 
   return (
     <>
+      <h1>Hacker Stories</h1>
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
+
+      <hr />
+      <List list={searchStories} />
+    </>
+  );
+}
+
+function Search(props) {
+  return (
+    <>
       <label htmlFor="search">Search</label>
-      <input
-        id="search"
-        type="text"
-        onChange={handleChange}
-        onMouseOver={handleChange}
-      />
+      <input id="search" type="text" onChange={props.onSearch} />
       <p>
-        Searching for <strong>{searchTerm}</strong>
+        Searching for <strong>{props.searchTerm}</strong>
       </p>
     </>
   );
 }
 
-function App() {
-  let todoList = [
-    {
-      id: 1234,
-      title: "Complete CTD assignment",
-    },
-    {
-      id: 4321,
-      title: "Go grocery shopping",
-    },
-    {
-      id: 9876,
-      title: "Go the gym",
-    },
-    {
-      id: 4987,
-      title: "Do laundry",
-    },
-  ];
-
-  const handleSearch = (event) => {
-    console.log(event.target.value);
-  };
-
-  return (
-    <>
-      <h1>Todo List</h1>
-      <Search onSearch={handleSearch} />
-      <List list={todoList} title="React Ecosystem" />
-    </>
-  );
-}
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
 
 export default App;
