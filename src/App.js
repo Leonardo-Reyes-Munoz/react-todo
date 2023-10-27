16221ff77741bedc24ed90f5c7a735617364195d
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-10-27 09:19:53 -0700

    refactor list array to TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-import React from "react";
-import AddTodoForm from "./AddTodoForm";
-import TodoList from "./TodoList";
+import React from 'react';
+import AddTodoForm from './AddTodoForm';
+import TodoList from './TodoList';
 
 function App() {
-  const todoList = [
-    {
-      id: 1234,
-      title: "Complete CTD assignment",
-    },
-    {
-      id: 4321,
-      title: "Go grocery shopping",
-    },
-    {
-      id: 9876,
-      title: "Go the gym",
-    },
-    {
-      id: 4987,
-      title: "Do laundry",
-    },
-  ];
-
-  const [newTodo, setNewTodo] = React.useState("");
+  const [newTodo, setNewTodo] = React.useState('');
 
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={setNewTodo} />
       <p>{newTodo}</p>
-      <TodoList list={todoList} />
+      <TodoList />
     </>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,29 @@
-import React from "react";
-import TodoListItem from "./TodoListItem";
+import React from 'react';
+import TodoListItem from './TodoListItem';
 
-function TodoList(props) {
+function TodoList() {
+  const todoList = [
+    {
+      id: 1234,
+      title: 'Complete CTD assignment',
+    },
+    {
+      id: 4321,
+      title: 'Go grocery shopping',
+    },
+    {
+      id: 9876,
+      title: 'Go the gym',
+    },
+    {
+      id: 4987,
+      title: 'Do laundry',
+    },
+  ];
+
   return (
     <ul>
-      {props.list.map((item) => (
+      {todoList.map((item) => (
         <TodoListItem key={item.id} todo={item.title} />
       ))}
     </ul>
