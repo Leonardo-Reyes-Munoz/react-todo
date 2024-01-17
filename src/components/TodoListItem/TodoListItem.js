d855c60c79e9517989c6f38eb6c049aa21a6a668
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2024-01-17 12:42:57 -0800

    style: add edit button and edit styling to align right

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -5,18 +5,22 @@ const TodoListItem = ({ todo, onRemoveTodo }) => {
   return (
     <React.Fragment>
       <div className={styles.TodoItem}>
-        <input id={todo.id} type="checkbox" />
-        <label htmlFor={todo.id}>{todo.title}</label>
-        <button
-          type="button"
-          className={` ${styles.remove}`}
-          onClick={() => onRemoveTodo(todo.id)}
-        >
-          <span className="material-symbols-outlined">delete</span>
-        </button>
-        <button type="button" className={` ${styles.edit}`}>
-          <span className="material-symbols-outlined">edit</span>
-        </button>
+        <div>
+          <input id={todo.id} type="checkbox" />
+          <label htmlFor={todo.id}>{todo.title}</label>
+        </div>
+        <div>
+          <button
+            type="button"
+            className={` ${styles.remove}`}
+            onClick={() => onRemoveTodo(todo.id)}
+          >
+            <span className="material-symbols-outlined">delete</span>
+          </button>
+          <button type="button" className={` ${styles.edit}`}>
+            <span className="material-symbols-outlined">edit</span>
+          </button>
+        </div>
       </div>
       <hr />
     </React.Fragment>
