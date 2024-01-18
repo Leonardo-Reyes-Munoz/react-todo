546a8437352319e26d48e0871d9fa37e94afa58e
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2024-01-18 09:44:40 -0800

    style: add icon to Nav title and navigation icons to Nav

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -4,22 +4,22 @@ export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <a href="/" className={styles.SiteTitle}>
-        Task-Tackler <span class="material-symbols-outlined">task_alt</span>
+        Task-Tackler <span className="material-symbols-outlined">task_alt</span>
       </a>
       <ul>
         <li>
           <a href="/shared">
-            <span class="material-symbols-outlined">groups</span>
+            <span className="material-symbols-outlined">groups</span>
           </a>
         </li>
         <li>
           <a href="/calendar">
-            <span class="material-symbols-outlined">calendar_month</span>
+            <span className="material-symbols-outlined">calendar_month</span>
           </a>
         </li>
         <li>
           <a href="/logout">
-            <span class="material-symbols-outlined">logout</span>
+            <span className="material-symbols-outlined">logout</span>
           </a>
         </li>
       </ul>
