aa1e100cef1ff81b9e538b2b57bb24f271140420
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-02-10 11:04:32 -0800

    style(Navbar.module.css): restore styling to logout button since it was changed from a link to a button.

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -16,16 +16,6 @@ export default function Navbar() {
         <span className="material-symbols-outlined task-logo">task_alt</span>
       </a>
       <ul>
-        {/* <li>
-          <a href="/shared">
-            <span className="material-symbols-outlined">groups</span>
-          </a>
-        </li>
-        <li>
-          <a href="/calendar">
-            <span className="material-symbols-outlined">calendar_month</span>
-          </a>
-        </li> */}
         <li>
           <button type="button" onClick={handleLogout} className="logout-btn">
             <span className="material-symbols-outlined">logout</span>
