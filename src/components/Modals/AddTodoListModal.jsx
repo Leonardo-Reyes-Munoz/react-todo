f47076d12e6dab431c8db2a2db6e9abfbc6e524f
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-02-22 22:31:38 -0800

    feat(AddTodoListModal): create modal to enable adding a TodoList.

---
+++ src/components/Modals/AddTodoListModal.jsx
@@ -0,0 +1,56 @@
+import './modal.css';
+import toast, { Toaster } from 'react-hot-toast';
+
+export default function AddTodoListModal({ onClose, loadTodoList }) {
+  async function handleAddSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+
+    const newTitle = formProps.newTitle;
+
+    const response = await createTodoItem(newTitle, updatedDueDate);
+    onClose();
+    loadTodoList();
+    toast.success(response);
+  }
+
+  return (
+    <div className="modal">
+      <Toaster />
+      <div className="overlay" onClick={onClose}></div>
+      <div className="modalContent">
+        <h2 className="modalTitle">Add New Task List</h2>
+        <form onSubmit={handleAddSubmit}>
+          <div className="inputItem">
+            <label htmlFor="newTitle">List name:</label>
+            <input type="text" name="newTitle" id="newTitle" />
+          </div>
+          <div className="color-picker">
+            <fieldset>
+              <legend>Choose a color</legend>
+              <input type="radio" name="color" value="yellow"></input>
+              <input type="radio" name="color" value="green"></input>
+              <input type="radio" name="color" value="orange"></input>
+              <input type="radio" name="color" value="purple"></input>
+            </fieldset>
+          </div>
+          <div className="btnContainer">
+            <button type="submit" className="submitEdit">
+              Add Task
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                onClose();
+              }}
+              className="cancel"
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
